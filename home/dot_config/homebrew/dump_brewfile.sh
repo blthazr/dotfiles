e73e6eafbcfd9740df9a125aee7439ecb9fe90d6
Chris Burnham <16470300+blthazr@users.noreply.github.com>
2025-06-09 19:37:44 -0500

    chore: update Homebrew

--- home/dot_config/homebrew/dump_brewfile.sh
+++ home/dot_config/homebrew/dump_brewfile.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-DOTFILE_BREWFILE_PATHS="home/dot_config/homebrew/Brewfile-bundle-dump-shard"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   @author         :   Chris Burnham
+#   @file           :   home/dot_config/homebrew/dump_brewfile.sh
+#   @description    :   Update Homebrew bundle file
+#   @usage          :   ./home/dot_config/homebrew/dump_brewfile.sh
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+set -u
+
+DOTFILE_BREWFILE_PATHS="home/dot_config/homebrew/Brewfile-bundle-dump"
 
 # dump homebrew bundle
 brew bundle dump --describe --force --file="${DOTFILE_BREWFILE_PATHS}"
